8bf30b4cd6664c457144e249d0c9062d8ad04dd0
Jérôme Blanchard <jblanchard@atilf.fr>
2014-10-28 14:31:12 +0000

    Adding JUnit Arquillian test for ToolService

---
+++ comp/src/test/java/fr/ortolang/diffusion/tool/ToolServiceTest.java
@@ -0,0 +1,154 @@
+package fr.ortolang.diffusion.tool;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.ejb.EJB;
+import javax.security.auth.login.LoginContext;
+import javax.security.auth.login.LoginException;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.EnterpriseArchive;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
+import org.jboss.shrinkwrap.resolver.api.maven.PomEquippedResolveStage;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import fr.ortolang.diffusion.membership.MembershipService;
+import fr.ortolang.diffusion.registry.KeyAlreadyExistsException;
+import fr.ortolang.diffusion.security.authentication.AuthenticationLoginContextFactory;
+import fr.ortolang.diffusion.security.authorisation.AccessDeniedException;
+
+@RunWith(Arquillian.class)
+public class ToolServiceTest {
+
+	private static Logger logger = Logger.getLogger(ToolServiceTest.class.getName());
+
+	@EJB
+	private MembershipService membership;
+	@EJB
+	private ToolService tool;
+
+	@Deployment
+	public static EnterpriseArchive createDeployment() {
+		JavaArchive jar = ShrinkWrap.create(JavaArchive.class, "diffusion-server-ejb.jar");
+		jar.addPackage("fr.ortolang.diffusion");
+		// jar.addPackage("fr.ortolang.diffusion.bootstrap");
+		jar.addPackage("fr.ortolang.diffusion");
+		jar.addPackage("fr.ortolang.diffusion.browser");
+		jar.addPackage("fr.ortolang.diffusion.event");
+		jar.addPackage("fr.ortolang.diffusion.event.entity");
+		jar.addClass("fr.ortolang.diffusion.indexing.IndexingService");
+		jar.addClass("fr.ortolang.diffusion.indexing.IndexingServiceBean");
+		jar.addClass("fr.ortolang.diffusion.indexing.IndexingServiceException");
+		jar.addPackage("fr.ortolang.diffusion.membership");
+		jar.addPackage("fr.ortolang.diffusion.membership.entity");
+		jar.addPackage("fr.ortolang.diffusion.notification");
+		jar.addPackage("fr.ortolang.diffusion.registry");
+		jar.addPackage("fr.ortolang.diffusion.registry.entity");
+		jar.addPackage("fr.ortolang.diffusion.security.authentication");
+		jar.addPackage("fr.ortolang.diffusion.security.authorisation");
+		jar.addPackage("fr.ortolang.diffusion.security.authorisation.entity");
+		jar.addPackage("fr.ortolang.diffusion.store.binary");
+		jar.addPackage("fr.ortolang.diffusion.store.binary.hash");
+		jar.addPackage("fr.ortolang.diffusion.store.index");
+		jar.addPackage("fr.ortolang.diffusion.store.triple");
+		jar.addPackage("fr.ortolang.diffusion.runtime");
+		jar.addPackage("fr.ortolang.diffusion.runtime.activiti");
+		jar.addPackage("fr.ortolang.diffusion.runtime.entity");
+		jar.addPackage("fr.ortolang.diffusion.runtime.task");
+		jar.addPackage("fr.ortolang.diffusion.tool");
+		jar.addPackage("fr.ortolang.diffusion.tool.entity");
+		jar.addPackage("fr.ortolang.diffusion.tool.invoke");
+		jar.addPackage("fr.ortolang.diffusion.tool.treetagger");
+		jar.addAsResource("config.properties");
+		jar.addAsResource("ontology/foaf.xml");
+		jar.addAsResource("ontology/ortolang.xml");
+		jar.addAsResource("ontology/rdfs.xml");
+		jar.addAsResource("test-activiti.cfg.xml", "activiti.cfg.xml");
+		jar.addAsResource("runtime/HelloWorld.bpmn");
+		jar.addAsManifestResource("test-persistence.xml", "persistence.xml");
+		logger.log(Level.INFO, "Created JAR for test : " + jar.toString(true));
+
+		PomEquippedResolveStage pom = Maven.resolver().loadPomFromFile("pom.xml");
+
+		EnterpriseArchive ear = ShrinkWrap.create(EnterpriseArchive.class, "diffusion-server-ear.ear");
+		ear.addAsModule(jar);
+		ear.addAsLibraries(pom.resolve("commons-io:commons-io:2.4").withTransitivity().asFile());
+		ear.addAsLibraries(pom.resolve("com.healthmarketscience.rmiio:rmiio:2.0.4").withTransitivity().asFile());
+		ear.addAsLibraries(pom.resolve("org.wildfly:wildfly-ejb-client-bom:pom:8.0.0.Final").withTransitivity().asFile());
+		ear.addAsLibraries(pom.resolve("org.apache.tika:tika-core:1.4").withTransitivity().asFile());
+		ear.addAsLibraries(pom.resolve("org.apache.lucene:lucene-core:4.6.0").withTransitivity().asFile());
+		ear.addAsLibraries(pom.resolve("org.apache.lucene:lucene-highlighter:4.6.0").withTransitivity().asFile());
+		ear.addAsLibraries(pom.resolve("org.apache.lucene:lucene-analyzers-common:4.6.0").withTransitivity().asFile());
+		ear.addAsLibraries(pom.resolve("org.apache.lucene:lucene-queryparser:4.6.0").withTransitivity().asFile());
+		ear.addAsLibraries(pom.resolve("org.openrdf.sesame:sesame-repository-api:2.7.8").withTransitivity().asFile());
+		ear.addAsLibraries(pom.resolve("org.openrdf.sesame:sesame-repository-sail:2.7.8").withTransitivity().asFile());
+		ear.addAsLibraries(pom.resolve("org.openrdf.sesame:sesame-sail-api:2.7.8").withTransitivity().asFile());
+		ear.addAsLibraries(pom.resolve("org.openrdf.sesame:sesame-sail-nativerdf:2.7.8").withTransitivity().asFile());
+		ear.addAsLibraries(pom.resolve("org.openrdf.sesame:sesame-model:2.7.8").withTransitivity().asFile());
+		ear.addAsLibraries(pom.resolve("org.openrdf.sesame:sesame-rio-api:2.7.8").withTransitivity().asFile());
+		ear.addAsLibraries(pom.resolve("org.openrdf.sesame:sesame-rio-rdfxml:2.7.8").withTransitivity().asFile());
+		ear.addAsLibraries(pom.resolve("org.openrdf.sesame:sesame-queryparser-sparql:2.7.8").withTransitivity().asFile());
+		ear.addAsLibraries(pom.resolve("org.openrdf.sesame:sesame-queryresultio-api:2.7.8").withTransitivity().asFile());
+		ear.addAsLibraries(pom.resolve("org.openrdf.sesame:sesame-queryresultio-sparqlxml:2.7.8").withTransitivity().asFile());
+		ear.addAsLibraries(pom.resolve("org.openrdf.sesame:sesame-queryresultio-sparqljson:2.7.8").withTransitivity().asFile());
+		ear.addAsLibraries(pom.resolve("org.apache.velocity:velocity:1.7").withTransitivity().asFile());
+		ear.addAsLibraries(pom.resolve("org.apache.velocity:velocity-tools:2.0").withTransitivity().asFile());
+		ear.addAsLibraries(pom.resolve("org.activiti:activiti-engine:5.16.3").withTransitivity().asFile());
+		ear.addAsLibraries(pom.resolve("org.springframework:spring-context:4.0.6.RELEASE").withTransitivity().asFile());
+		logger.log(Level.INFO, "Created EAR for test : " + ear.toString(true));
+
+		return ear;
+	}
+
+	@Before
+	public void setup() {
+		logger.log(Level.INFO, "setting up test environment");
+	}
+
+	@After
+	public void tearDown() {
+		logger.log(Level.INFO, "clearing environment");
+	}
+
+	@Test
+	public void testLogin() throws LoginException {
+		LoginContext loginContext = AuthenticationLoginContextFactory.createLoginContext("guest", "password");
+		loginContext.login();
+		try {
+			logger.log(Level.INFO, membership.getProfileKeyForConnectedIdentifier());
+			String key = membership.getProfileKeyForConnectedIdentifier();
+			assertEquals("guest", key);
+		} finally {
+			loginContext.logout();
+		}
+	}
+
+	@Test
+	public void testDeclareTool() throws LoginException, ToolServiceException, KeyAlreadyExistsException, AccessDeniedException {
+		LoginContext loginContext = AuthenticationLoginContextFactory.createLoginContext("guest", "password");
+		loginContext.login();
+		try {
+			logger.log(Level.INFO, membership.getProfileKeyForConnectedIdentifier());
+			tool.declareTool("K1", "treetagger", "This is Tree Tagger", "TreeTagger is a dedicated tool for text treatment et blabla bla...", "fr.ortolang.diffusion.tool.treetagger.TreeTaggerInvoker");
+			
+			assertTrue(tool.listTools().size() == 1);
+			
+		} catch ( ToolServiceException e )  {
+			fail("Should have raised an AccessDeniedException");
+		} finally {
+			loginContext.logout();
+		}
+	}
+
+}
