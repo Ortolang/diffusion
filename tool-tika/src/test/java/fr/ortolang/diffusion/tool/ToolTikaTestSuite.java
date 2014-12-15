9254367540c7c9f7e0003fc141d7e207e8cbff84
cmoro <cmoro@atifl.fr>
2014-12-15 11:23:51 +0100

    fix error in test suite

--- tool-tika/src/test/java/fr/ortolang/diffusion/tool/ToolTikaTestSuite.java
+++ tool-tika/src/test/java/fr/ortolang/diffusion/tool/ToolTikaTestSuite.java
@@ -5,15 +5,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import fr.ortolang.diffusion.api.OrtolangObjectidentifierTest;
-import fr.ortolang.diffusion.core.CollectionUnitTest;
-import fr.ortolang.diffusion.core.CoreServiceTest;
-import fr.ortolang.diffusion.core.PathBuilderTest;
-import fr.ortolang.diffusion.registry.RegistryServiceTest;
-import fr.ortolang.diffusion.store.binary.BinaryStoreServiceTest;
-import fr.ortolang.diffusion.store.index.IndexStoreServiceTest;
-import fr.ortolang.diffusion.store.triple.TripleStoreServiceTest;
-
 
 @RunWith(Suite.class)
 @SuiteClasses({ })
