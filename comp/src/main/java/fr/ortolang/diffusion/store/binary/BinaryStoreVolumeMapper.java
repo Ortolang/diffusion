a46668e0c4a5a5578c97516f4a2f5f2f2659d55b
Jérôme Blanchard <jayblanc@gmail.com>
2015-01-16 12:35:35 +0100

    Adding BinaryStore shrading using volumes mapping

---
+++ comp/src/main/java/fr/ortolang/diffusion/store/binary/BinaryStoreVolumeMapper.java
@@ -0,0 +1,42 @@
+package fr.ortolang.diffusion.store.binary;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BinaryStoreVolumeMapper {
+	
+	private static final String PREFIX = "volume";
+	
+	private static Map<String, String> mapping = new HashMap<String, String> ();
+	private static List<String> volumes = new ArrayList<String> ();
+	
+	static {
+		int n = Integer.parseInt("10", 16);
+		int v = 1;
+		while ( n <= Integer.parseInt("ff", 16) ) {
+			volumes.add(PREFIX + v);
+			for (int i=0; i<31; i++) {
+				if ( n <= Integer.parseInt("ff", 16) ) {
+					mapping.put(Integer.toHexString(n), PREFIX + v);
+					n++;
+				}
+			}
+			v++;
+		}
+	}
+	
+	public static String getVolume(String digit) {
+		return mapping.get(digit);
+	}
+	
+	public static List<String> listVolumes() {
+		return volumes;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(BinaryStoreVolumeMapper.getVolume("98"));
+	}
+
+}
