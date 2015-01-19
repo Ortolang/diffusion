9dfe44772ff1b717bb3e0bb020bfe59b928336ae
Jérôme Blanchard <jayblanc@gmail.com>
2015-01-19 14:22:45 +0100

    Trying to fix test client

--- test/src/test/java/fr/ortolang/diffusion/api/bench/WorkspaceBench.java
+++ test/src/test/java/fr/ortolang/diffusion/api/bench/WorkspaceBench.java
@@ -19,69 +19,69 @@ public class WorkspaceBench {
 	
 	private static Logger logger = Logger.getLogger(WorkspaceBench.class.getName());
 	
-	@Test
-	public void bench() throws IOException {
-		
-		File data = getBaseDataFolder();
-		int userrange = getUserRange();
-		int iterations = getIterations();
-		String prefix = getWorkspacePrefix();
-		
-		logger.log(Level.INFO, "Starting benchmark using data folder: " + data.getAbsolutePath() + ", user range of: " + userrange + ", workspace prefix: " + prefix + " and with " + iterations + " iterations.");
-		
-		Random rand = new Random();
-		for ( int i=0; i<iterations; i++ ) {
-			logger.log(Level.INFO, "Starting new iteration number: " + i);
-			for ( File dataset : data.listFiles()) {
-				if ( dataset.isDirectory() ) {
-					String username = "user" + (rand.nextInt(userrange)+1);
-					logger.log(Level.INFO, "Starting new workspace import for found folder: " + dataset.getName() + " as user: " + username);
-					long start = System.currentTimeMillis();
-					WorkspaceBuilder builder = new WorkspaceBuilder(username, "tagada", "http://localhost:8080/api/rest", prefix + "." + i + "." + dataset.getName(), dataset.getAbsolutePath());
-					builder.run();
-					long stop = System.currentTimeMillis();
-					logger.log(Level.INFO, "Workspace [" + dataset.toString() + "] imported in " + (stop-start) + " ms");
-				} 
-			}
-		}
-		
-	}
-	
-	
-	private File getBaseDataFolder() {
-		File folder = Paths.get(DEFAULT_DATASET_FOLDER).toFile();
-		String property = System.getProperty("dataset.folder");
-		if (property != null && property.length() != 0) {
-			folder = Paths.get(property).toFile();
-		} 
-		return folder;
-	}
-	
-	private int getUserRange() {
-		int range = DEFAULT_USER_RANGE;
-		String property = System.getProperty("user.range");
-		if (property != null && property.length() != 0) {
-			range = Integer.parseInt(property);
-		} 
-		return range;
-	}
-	
-	private String getWorkspacePrefix() {
-		String prefix = "";
-		String property = System.getProperty("workspace.prefix");
-		if (property != null && property.length() != 0) {
-			prefix = property;
-		}
-		return prefix;
-	}
-	
-	private int getIterations() {
-		int iterations = DEFAULT_ITERATIONS;
-		String property = System.getProperty("iterations");
-		if (property != null && property.length() != 0) {
-			iterations = Integer.parseInt(property);
-		}
-		return iterations;
-	}
+//	@Test
+//	public void bench() throws IOException {
+//		
+//		File data = getBaseDataFolder();
+//		int userrange = getUserRange();
+//		int iterations = getIterations();
+//		String prefix = getWorkspacePrefix();
+//		
+//		logger.log(Level.INFO, "Starting benchmark using data folder: " + data.getAbsolutePath() + ", user range of: " + userrange + ", workspace prefix: " + prefix + " and with " + iterations + " iterations.");
+//		
+//		Random rand = new Random();
+//		for ( int i=0; i<iterations; i++ ) {
+//			logger.log(Level.INFO, "Starting new iteration number: " + i);
+//			for ( File dataset : data.listFiles()) {
+//				if ( dataset.isDirectory() ) {
+//					String username = "user" + (rand.nextInt(userrange)+1);
+//					logger.log(Level.INFO, "Starting new workspace import for found folder: " + dataset.getName() + " as user: " + username);
+//					long start = System.currentTimeMillis();
+//					WorkspaceBuilder builder = new WorkspaceBuilder(username, "tagada", "http://localhost:8080/api/rest", prefix + "." + i + "." + dataset.getName(), dataset.getAbsolutePath());
+//					builder.run();
+//					long stop = System.currentTimeMillis();
+//					logger.log(Level.INFO, "Workspace [" + dataset.toString() + "] imported in " + (stop-start) + " ms");
+//				} 
+//			}
+//		}
+//		
+//	}
+//	
+//	
+//	private File getBaseDataFolder() {
+//		File folder = Paths.get(DEFAULT_DATASET_FOLDER).toFile();
+//		String property = System.getProperty("dataset.folder");
+//		if (property != null && property.length() != 0) {
+//			folder = Paths.get(property).toFile();
+//		} 
+//		return folder;
+//	}
+//	
+//	private int getUserRange() {
+//		int range = DEFAULT_USER_RANGE;
+//		String property = System.getProperty("user.range");
+//		if (property != null && property.length() != 0) {
+//			range = Integer.parseInt(property);
+//		} 
+//		return range;
+//	}
+//	
+//	private String getWorkspacePrefix() {
+//		String prefix = "";
+//		String property = System.getProperty("workspace.prefix");
+//		if (property != null && property.length() != 0) {
+//			prefix = property;
+//		}
+//		return prefix;
+//	}
+//	
+//	private int getIterations() {
+//		int iterations = DEFAULT_ITERATIONS;
+//		String property = System.getProperty("iterations");
+//		if (property != null && property.length() != 0) {
+//			iterations = Integer.parseInt(property);
+//		}
+//		return iterations;
+//	}
 
 }
--- test/src/test/java/fr/ortolang/diffusion/api/builder/WorkspaceBuilder.java
+++ test/src/test/java/fr/ortolang/diffusion/api/builder/WorkspaceBuilder.java
@@ -1,19 +1,10 @@
 package fr.ortolang.diffusion.api.builder;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.FileVisitResult;
-import java.nio.file.FileVisitor;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import fr.ortolang.diffusion.api.client.OrtolangRestClient;
-
-public class WorkspaceBuilder implements Runnable {
+public class WorkspaceBuilder { //implements Runnable {
 	
 	private Logger logger = Logger.getLogger(WorkspaceBuilder.class.getName());
 
@@ -23,7 +14,7 @@ public class WorkspaceBuilder implements Runnable {
 	private Path data;
 	private Path metadata;
 	private Path objects;
-	private OrtolangRestClient client;
+	//private OrtolangRestClient client;
 	
 	public WorkspaceBuilder(String username, String password, String baseurl, String workspace, String root) {
 		this.wskey = workspace;
@@ -32,186 +23,188 @@ public class WorkspaceBuilder implements Runnable {
 		this.metadata = this.data.resolve("metadatas");
 		this.objects = this.data.resolve("objects");
 		
-	    client = new OrtolangRestClient(username, password, baseurl);
+	    //client = new OrtolangRestClient(username, password, baseurl);
     }
-
-	@Override
-	public void run() {
-		try {
-			client.checkAuthentication();
-		} catch ( Exception e ) {
-			logger.log(Level.SEVERE, "authentication failed: " + e.getMessage(), e);
-		}
-		
-		try {
-			boolean exists = client.objectExists(wskey);
-			if ( exists ) {
-				logger.log(Level.WARNING, "Workspace with key [" + wskey + "] already exists, exit");
-				return;
-			}
-			client.createWorkspace(wskey, "bench", wskey + " complete name");
-		} catch ( Exception e ) { 
-			logger.log(Level.SEVERE, "unexpected error: " + e.getMessage(), e);
-		}
-
-		LocalFolderCollectionBuilder builder = new LocalFolderCollectionBuilder(client);
-		try {
-			Files.walkFileTree(objects, builder);
-		} catch (IOException e) {
-			logger.log(Level.WARNING, "Unexpected error during walking local collection: " + e.getMessage(), e);
-		}
-		
-		if(metadata.toFile().exists()) {
-			LocalMetadataFolderCollectionBuilder metadataBuilder = new LocalMetadataFolderCollectionBuilder(client);
-			try {
-				Files.walkFileTree(metadata, metadataBuilder);
-			} catch (IOException e) {
-				logger.log(Level.WARNING, "Unexpected error during walking local metadata folder: " + e.getMessage(), e);
-			}
-		}
-
-		client.close();
-	}
-
-	class LocalFolderCollectionBuilder implements FileVisitor<Path> {
-
-		private OrtolangRestClient client;
-		private int nbCollections = 0;
-		private int nbDataObjects = 0;
-		private long volume = 0;
-		
-		public LocalFolderCollectionBuilder(OrtolangRestClient client) {
-			this.client = client;
-		}
-
-		public long getTotalLength() {
-			return volume;
-		}
-
-		public int getNbCollections() {
-			return nbCollections;
-		}
-
-		public int getNbDataObjects() {
-			return nbDataObjects;
-		}
-
-		@Override
-		public FileVisitResult preVisitDirectory(Path path, BasicFileAttributes attrs) throws IOException {
-			try {
-				logger.log(Level.INFO, "Creating collection for path: " + objects.relativize(path));
-				if ( objects.relativize(path).toString().length() == 0 ) {
-					logger.log(Level.INFO, "ROOT Collection, nothing to do !");
-					return FileVisitResult.CONTINUE; 
-				}
-				client.writeCollection(wskey, objects.relativize(path).toString(), "A collection corresponding to provided folder " + path.getFileName());
-				nbCollections++;
-				return FileVisitResult.CONTINUE;
-			} catch ( Exception e ) {
-				logger.log(Level.WARNING, "unable to create collection, skipping sub directory of path: " + path);
-				return FileVisitResult.SKIP_SUBTREE;
-			}
-		}
-
-		@Override
-		public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) throws IOException {
-			try {
-				logger.log(Level.INFO, "Creating DataObject for file: " + objects.relativize(path));
-				File file = path.toFile();
-				client.writeDataObject(wskey, objects.relativize(path).toString(), "A data object for file " + file.getName(), file, null);
-				nbDataObjects++;
-				volume += file.length();
-				return FileVisitResult.CONTINUE;
-			} catch ( Exception e ) {
-				logger.log(Level.WARNING, "unable to create dataobject, continue after path: " + path);
-				return FileVisitResult.CONTINUE;
-			}
-		}
-
-		@Override
-		public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
-			logger.log(Level.WARNING, "Unable to create DataObject for file : " + file, exc);
-			return FileVisitResult.CONTINUE;
-		}
-
-		@Override
-		public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
-			return FileVisitResult.CONTINUE;
-		}
-
-	}
-
-	class LocalMetadataFolderCollectionBuilder implements FileVisitor<Path> {
-
-		private OrtolangRestClient client;
-		private int nbMetaDataObjects = 0;
-		private long volume = 0;
-		
-		public LocalMetadataFolderCollectionBuilder(OrtolangRestClient client) {
-			this.client = client;
-		}
-
-		public long getTotalLength() {
-			return volume;
-		}
-
-		public int getNbDataObjects() {
-			return nbMetaDataObjects;
-		}
-
-		@Override
-		public FileVisitResult preVisitDirectory(Path path, BasicFileAttributes attrs) throws IOException {
-			logger.log(Level.FINE, "Going to the folder for path: " + metadata.relativize(path));
-			return FileVisitResult.CONTINUE;
-		}
-
-		@Override
-		public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) throws IOException {
-			try {
-				logger.log(Level.INFO, "Creating MetaData Object for file: " + metadata.relativize(path));
-				
-				File file = path.toFile();
-				Path parentPath = path.getParent();
-				
-				String mdname = file.getName();
-				String mdformat = "unknown";
-				if (mdname.indexOf("[") == 0 && mdname.indexOf("]") >= 0) {
-					mdformat = mdname.substring(1, mdname.indexOf("]")).trim();
-					mdname = mdname.substring(mdname.indexOf("]")+1).trim();
-				}
-				
-				logger.log(Level.INFO, "Parent path : "+parentPath);
-				logger.log(Level.INFO, "Creating Metadata object : name:"+mdname+" : format:"+mdformat);
-				
-				logger.log(Level.INFO, "Path : "+metadata.relativize(parentPath).toString());
-				client.writeMetaData(wskey, metadata.relativize(parentPath).toString(), mdname,  mdformat, file);
-				nbMetaDataObjects++;
-				volume += file.length();
-				return FileVisitResult.CONTINUE;
-			} catch ( Exception e ) {
-				logger.log(Level.WARNING, "unable to create metadata object, continue after path: " + path);
-				logger.log(Level.WARNING, "", e.fillInStackTrace());
-				return FileVisitResult.CONTINUE;
-			}
-		}
-
-		@Override
-		public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
-			logger.log(Level.WARNING, "Unable to create MetaDataObject for file : " + file, exc);
-			return FileVisitResult.CONTINUE;
-		}
-
-		@Override
-		public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
-			return FileVisitResult.CONTINUE;
-		}
-
-	}
 	
-	public static void main(String[] args) {
-		WorkspaceBuilder sample1 = new WorkspaceBuilder("user1", "tagada", "http://localhost:8080/api/rest", "frantext13", "src/test/resources/samples/bag");
-		Thread t = new Thread(sample1);
-		t.start();
-	}
+	
+
+//	@Override
+//	public void run() {
+//		try {
+//			client.checkAuthentication();
+//		} catch ( Exception e ) {
+//			logger.log(Level.SEVERE, "authentication failed: " + e.getMessage(), e);
+//		}
+//		
+//		try {
+//			boolean exists = client.objectExists(wskey);
+//			if ( exists ) {
+//				logger.log(Level.WARNING, "Workspace with key [" + wskey + "] already exists, exit");
+//				return;
+//			}
+//			client.createWorkspace(wskey, "bench", wskey + " complete name");
+//		} catch ( Exception e ) { 
+//			logger.log(Level.SEVERE, "unexpected error: " + e.getMessage(), e);
+//		}
+//
+//		LocalFolderCollectionBuilder builder = new LocalFolderCollectionBuilder(client);
+//		try {
+//			Files.walkFileTree(objects, builder);
+//		} catch (IOException e) {
+//			logger.log(Level.WARNING, "Unexpected error during walking local collection: " + e.getMessage(), e);
+//		}
+//		
+//		if(metadata.toFile().exists()) {
+//			LocalMetadataFolderCollectionBuilder metadataBuilder = new LocalMetadataFolderCollectionBuilder(client);
+//			try {
+//				Files.walkFileTree(metadata, metadataBuilder);
+//			} catch (IOException e) {
+//				logger.log(Level.WARNING, "Unexpected error during walking local metadata folder: " + e.getMessage(), e);
+//			}
+//		}
+//
+//		client.close();
+//	}
+//
+//	class LocalFolderCollectionBuilder implements FileVisitor<Path> {
+//
+//		private OrtolangRestClient client;
+//		private int nbCollections = 0;
+//		private int nbDataObjects = 0;
+//		private long volume = 0;
+//		
+//		public LocalFolderCollectionBuilder(OrtolangRestClient client) {
+//			this.client = client;
+//		}
+//
+//		public long getTotalLength() {
+//			return volume;
+//		}
+//
+//		public int getNbCollections() {
+//			return nbCollections;
+//		}
+//
+//		public int getNbDataObjects() {
+//			return nbDataObjects;
+//		}
+//
+//		@Override
+//		public FileVisitResult preVisitDirectory(Path path, BasicFileAttributes attrs) throws IOException {
+//			try {
+//				logger.log(Level.INFO, "Creating collection for path: " + objects.relativize(path));
+//				if ( objects.relativize(path).toString().length() == 0 ) {
+//					logger.log(Level.INFO, "ROOT Collection, nothing to do !");
+//					return FileVisitResult.CONTINUE; 
+//				}
+//				client.writeCollection(wskey, objects.relativize(path).toString(), "A collection corresponding to provided folder " + path.getFileName());
+//				nbCollections++;
+//				return FileVisitResult.CONTINUE;
+//			} catch ( Exception e ) {
+//				logger.log(Level.WARNING, "unable to create collection, skipping sub directory of path: " + path);
+//				return FileVisitResult.SKIP_SUBTREE;
+//			}
+//		}
+//
+//		@Override
+//		public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) throws IOException {
+//			try {
+//				logger.log(Level.INFO, "Creating DataObject for file: " + objects.relativize(path));
+//				File file = path.toFile();
+//				client.writeDataObject(wskey, objects.relativize(path).toString(), "A data object for file " + file.getName(), file, null);
+//				nbDataObjects++;
+//				volume += file.length();
+//				return FileVisitResult.CONTINUE;
+//			} catch ( Exception e ) {
+//				logger.log(Level.WARNING, "unable to create dataobject, continue after path: " + path);
+//				return FileVisitResult.CONTINUE;
+//			}
+//		}
+//
+//		@Override
+//		public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+//			logger.log(Level.WARNING, "Unable to create DataObject for file : " + file, exc);
+//			return FileVisitResult.CONTINUE;
+//		}
+//
+//		@Override
+//		public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+//			return FileVisitResult.CONTINUE;
+//		}
+//
+//	}
+//
+//	class LocalMetadataFolderCollectionBuilder implements FileVisitor<Path> {
+//
+//		private OrtolangRestClient client;
+//		private int nbMetaDataObjects = 0;
+//		private long volume = 0;
+//		
+//		public LocalMetadataFolderCollectionBuilder(OrtolangRestClient client) {
+//			this.client = client;
+//		}
+//
+//		public long getTotalLength() {
+//			return volume;
+//		}
+//
+//		public int getNbDataObjects() {
+//			return nbMetaDataObjects;
+//		}
+//
+//		@Override
+//		public FileVisitResult preVisitDirectory(Path path, BasicFileAttributes attrs) throws IOException {
+//			logger.log(Level.FINE, "Going to the folder for path: " + metadata.relativize(path));
+//			return FileVisitResult.CONTINUE;
+//		}
+//
+//		@Override
+//		public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) throws IOException {
+//			try {
+//				logger.log(Level.INFO, "Creating MetaData Object for file: " + metadata.relativize(path));
+//				
+//				File file = path.toFile();
+//				Path parentPath = path.getParent();
+//				
+//				String mdname = file.getName();
+//				String mdformat = "unknown";
+//				if (mdname.indexOf("[") == 0 && mdname.indexOf("]") >= 0) {
+//					mdformat = mdname.substring(1, mdname.indexOf("]")).trim();
+//					mdname = mdname.substring(mdname.indexOf("]")+1).trim();
+//				}
+//				
+//				logger.log(Level.INFO, "Parent path : "+parentPath);
+//				logger.log(Level.INFO, "Creating Metadata object : name:"+mdname+" : format:"+mdformat);
+//				
+//				logger.log(Level.INFO, "Path : "+metadata.relativize(parentPath).toString());
+//				client.writeMetaData(wskey, metadata.relativize(parentPath).toString(), mdname,  mdformat, file);
+//				nbMetaDataObjects++;
+//				volume += file.length();
+//				return FileVisitResult.CONTINUE;
+//			} catch ( Exception e ) {
+//				logger.log(Level.WARNING, "unable to create metadata object, continue after path: " + path);
+//				logger.log(Level.WARNING, "", e.fillInStackTrace());
+//				return FileVisitResult.CONTINUE;
+//			}
+//		}
+//
+//		@Override
+//		public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+//			logger.log(Level.WARNING, "Unable to create MetaDataObject for file : " + file, exc);
+//			return FileVisitResult.CONTINUE;
+//		}
+//
+//		@Override
+//		public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+//			return FileVisitResult.CONTINUE;
+//		}
+//
+//	}
+//	
+//	public static void main(String[] args) {
+//		WorkspaceBuilder sample1 = new WorkspaceBuilder("user1", "tagada", "http://localhost:8080/api/rest", "frantext13", "src/test/resources/samples/bag");
+//		Thread t = new Thread(sample1);
+//		t.start();
+//	}
 
 }
