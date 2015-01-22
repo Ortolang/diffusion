cc5ea98573a4d6f79504d56436ef7db6e9f89b44
Jérôme Blanchard <jayblanc@gmail.com>
2015-01-22 17:04:54 +0100

    Removing test purpose code

--- comp/src/main/java/fr/ortolang/diffusion/security/authentication/AuthenticationServiceBean.java
+++ comp/src/main/java/fr/ortolang/diffusion/security/authentication/AuthenticationServiceBean.java
@@ -26,11 +26,6 @@ public class AuthenticationServiceBean implements AuthenticationService {
 	@Override
 	public String getConnectedIdentifier() {
 		logger.log(Level.FINE, "Connected identifier " + ctx.getCallerPrincipal().getName());
-		if ( ctx.isCallerInRole("tool") ) {
-			logger.log(Level.INFO, "User is in role tool");
-		} else {
-			logger.log(Level.INFO, "User is NOT NOT in role tool");
-		}
 		return ctx.getCallerPrincipal().getName();
 	}
 
