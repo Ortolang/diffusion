9a1cc8ed3c60ac3aefd97ba5be594342c13f8f42
Frédéric Pierre <fpierre@atilf.fr>
2015-03-13 15:44:37 +0100

    - Add method to calculate collection size (returns: size, element number, collection number) and add resource in REST API - Remove default value in WorkspaceElementFormRepresentation

--- comp/src/main/java/fr/ortolang/diffusion/core/CoreServiceBean.java
+++ comp/src/main/java/fr/ortolang/diffusion/core/CoreServiceBean.java
@@ -36,59 +36,14 @@ package fr.ortolang.diffusion.core;
  * #L%
  */
 
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.UUID;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import javax.annotation.Resource;
-import javax.annotation.security.PermitAll;
-import javax.ejb.EJB;
-import javax.ejb.Local;
-import javax.ejb.SessionContext;
-import javax.ejb.Stateless;
-import javax.ejb.TransactionAttribute;
-import javax.ejb.TransactionAttributeType;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
-
-import org.jboss.ejb3.annotation.SecurityDomain;
-
-import fr.ortolang.diffusion.OrtolangEvent;
-import fr.ortolang.diffusion.OrtolangException;
-import fr.ortolang.diffusion.OrtolangIndexablePlainTextContent;
-import fr.ortolang.diffusion.OrtolangIndexableSemanticContent;
-import fr.ortolang.diffusion.OrtolangObject;
-import fr.ortolang.diffusion.OrtolangObjectIdentifier;
+import fr.ortolang.diffusion.*;
 import fr.ortolang.diffusion.core.entity.Collection;
-import fr.ortolang.diffusion.core.entity.CollectionElement;
-import fr.ortolang.diffusion.core.entity.DataObject;
-import fr.ortolang.diffusion.core.entity.Link;
-import fr.ortolang.diffusion.core.entity.MetadataElement;
-import fr.ortolang.diffusion.core.entity.MetadataObject;
-import fr.ortolang.diffusion.core.entity.SnapshotElement;
-import fr.ortolang.diffusion.core.entity.Workspace;
-import fr.ortolang.diffusion.core.entity.WorkspaceAlias;
+import fr.ortolang.diffusion.core.entity.*;
 import fr.ortolang.diffusion.membership.MembershipService;
 import fr.ortolang.diffusion.membership.MembershipServiceException;
 import fr.ortolang.diffusion.notification.NotificationService;
 import fr.ortolang.diffusion.notification.NotificationServiceException;
-import fr.ortolang.diffusion.registry.IdentifierAlreadyRegisteredException;
-import fr.ortolang.diffusion.registry.IdentifierNotRegisteredException;
-import fr.ortolang.diffusion.registry.KeyAlreadyExistsException;
-import fr.ortolang.diffusion.registry.KeyLockedException;
-import fr.ortolang.diffusion.registry.KeyNotFoundException;
-import fr.ortolang.diffusion.registry.RegistryService;
-import fr.ortolang.diffusion.registry.RegistryServiceException;
+import fr.ortolang.diffusion.registry.*;
 import fr.ortolang.diffusion.security.authorisation.AccessDeniedException;
 import fr.ortolang.diffusion.security.authorisation.AuthorisationService;
 import fr.ortolang.diffusion.security.authorisation.AuthorisationServiceException;
@@ -99,6 +54,18 @@ import fr.ortolang.diffusion.store.binary.DataNotFoundException;
 import fr.ortolang.diffusion.store.triple.Triple;
 import fr.ortolang.diffusion.store.triple.TripleStoreServiceException;
 import fr.ortolang.diffusion.store.triple.URIHelper;
+import org.jboss.ejb3.annotation.SecurityDomain;
+
+import javax.annotation.Resource;
+import javax.annotation.security.PermitAll;
+import javax.ejb.*;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.io.InputStream;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Local(CoreService.class)
 @Stateless(name = CoreService.SERVICE_NAME)
@@ -665,7 +632,23 @@ public class CoreServiceBean implements CoreService {
 		}
 	}
 
-	@Override
+    @Override
+    @TransactionAttribute(TransactionAttributeType.SUPPORTS)
+    public OrtolangSizeInfo calculateCollectionSize(String key) throws CoreServiceException, KeyNotFoundException, AccessDeniedException {
+        logger.log(Level.FINE, "calculating collection size for collection with key [" + key + "]");
+        try {
+            List<String> subjects = membership.getConnectedIdentifierSubjects();
+            OrtolangSizeInfo ortolangSizeInfo = new OrtolangSizeInfo();
+
+            ortolangSizeInfo = calculateCollectionSize(key, ortolangSizeInfo, subjects);
+            return ortolangSizeInfo;
+        } catch (MembershipServiceException | RegistryServiceException e) {
+            logger.log(Level.SEVERE, "unexpected error while calculating collection size", e);
+            throw new CoreServiceException("unable to calculate collection size for collection with key [" + key + "]", e);
+        }
+    }
+
+    @Override
 	@TransactionAttribute(TransactionAttributeType.REQUIRED)
 	public String resolvePathFromCollection(String key, String path) throws KeyNotFoundException, CoreServiceException, AccessDeniedException, InvalidPathException {
 		logger.log(Level.FINE, "reading collection with key [" + key + "]");
@@ -2871,6 +2854,37 @@ public class CoreServiceBean implements CoreService {
 			throw new CloneException("unable to clone metadata with origin [" + origin + "] and target [" + target + "]", e);
 		}
 	}
+
+    @TransactionAttribute(TransactionAttributeType.SUPPORTS)
+    private OrtolangSizeInfo calculateCollectionSize(String key, OrtolangSizeInfo ortolangSizeInfo, List<String> subjects) throws KeyNotFoundException, RegistryServiceException, CoreServiceException {
+        logger.log(Level.FINE, "calculating collection size for collection with key [" + key + "]");
+        try {
+            OrtolangObjectIdentifier cidentifier = registry.lookup(key);
+            checkObjectType(cidentifier, Collection.OBJECT_TYPE);
+            authorisation.checkPermission(key, subjects, "read");
+
+            Collection collection = em.find(Collection.class, cidentifier.getId());
+            if (collection == null) {
+                throw new CoreServiceException("unable to load collection with id [" + cidentifier.getId() + "] from storage");
+            }
+
+            for (CollectionElement element : collection.getElements()) {
+                if (element.getType().equals(DataObject.OBJECT_TYPE)) {
+                    try {
+                        authorisation.checkPermission(element.getKey(), subjects, "read");
+                        ortolangSizeInfo.addElementSize(element.getSize());
+                    }  catch (AuthorisationServiceException | AccessDeniedException e) {
+                        ortolangSizeInfo.setPartial(true);
+                    }
+                } else if (element.getType().equals(Collection.OBJECT_TYPE)) {
+                    ortolangSizeInfo.incrementCollectionNumber();
+                    ortolangSizeInfo = calculateCollectionSize(element.getKey(), ortolangSizeInfo, subjects);
+                }
+            }
+        } catch (AuthorisationServiceException | AccessDeniedException e) {
+        }
+        return ortolangSizeInfo;
+    }
 	
 	@TransactionAttribute(TransactionAttributeType.REQUIRED)
 	private void deleteCollectionContent(Collection collection, int clock) throws CoreServiceException, RegistryServiceException, KeyNotFoundException, KeyLockedException {
