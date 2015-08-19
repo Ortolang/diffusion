dfede5dfc3473512ee3532c109c7c6ad0f21e70a
Jérôme Blanchard <jayblanc@gmail.com>
2015-08-19 10:09:37 +0200

    fix small bug

--- comp/src/test/java/fr/ortolang/diffusion/membership/MembershipServiceTest.java
+++ comp/src/test/java/fr/ortolang/diffusion/membership/MembershipServiceTest.java
@@ -59,7 +59,7 @@ public class MembershipServiceTest {
 		jar.addPackage("fr.ortolang.diffusion.browser");
 		jar.addPackage("fr.ortolang.diffusion.event");
 		jar.addPackage("fr.ortolang.diffusion.event.entity");
-		jar.addClass("fr.ortolang.diffusion.indexing");
+		jar.addPackage("fr.ortolang.diffusion.indexing");
 		jar.addPackage("fr.ortolang.diffusion.membership");
 		jar.addPackage("fr.ortolang.diffusion.membership.entity");
 		jar.addPackage("fr.ortolang.diffusion.notification");
