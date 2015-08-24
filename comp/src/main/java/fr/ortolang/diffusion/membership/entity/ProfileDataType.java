db6189c428799ffd3db52978e587ba137f9d3705
Frédéric Pierre <fpierre@atilf.fr>
2015-08-24 10:15:41 +0200

    - Fix website url edition in profile page (Closes #435)

--- comp/src/main/java/fr/ortolang/diffusion/membership/entity/ProfileDataType.java
+++ comp/src/main/java/fr/ortolang/diffusion/membership/entity/ProfileDataType.java
@@ -1,13 +1,14 @@
 package fr.ortolang.diffusion.membership.entity;
 
 public enum ProfileDataType {
-	BOOLEAN,
-	STRING,
-	TEXT,
-	EMAIL,
-	ADDRESS,
-	TEL,
-	MULTI_VALUE,
-	ENUM,
-	RADIO
+    BOOLEAN,
+    STRING,
+    TEXT,
+    EMAIL,
+    ADDRESS,
+    TEL,
+    MULTI_VALUE,
+    ENUM,
+    RADIO,
+    URL
 }
