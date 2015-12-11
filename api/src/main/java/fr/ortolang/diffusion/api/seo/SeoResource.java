ec0c31d4fc0732698790a84a567d3a5c58c1a228
Frédéric Pierre <fpierre@atilf.fr>
2015-12-11 11:07:23 +0100

    - Add SeoService and SeoResource - Update ortolang config (version 8)

---
+++ api/src/main/java/fr/ortolang/diffusion/api/seo/SeoResource.java
@@ -0,0 +1,73 @@
+package fr.ortolang.diffusion.api.seo;
+
+/*
+ * #%L
+ * ORTOLANG
+ * A online network structure for hosting language resources and tools.
+ *
+ * Jean-Marie Pierrel / ATILF UMR 7118 - CNRS / Université de Lorraine
+ * Etienne Petitjean / ATILF UMR 7118 - CNRS
+ * Jérôme Blanchard / ATILF UMR 7118 - CNRS
+ * Bertrand Gaiffe / ATILF UMR 7118 - CNRS
+ * Cyril Pestel / ATILF UMR 7118 - CNRS
+ * Marie Tonnelier / ATILF UMR 7118 - CNRS
+ * Ulrike Fleury / ATILF UMR 7118 - CNRS
+ * Frédéric Pierre / ATILF UMR 7118 - CNRS
+ * Céline Moro / ATILF UMR 7118 - CNRS
+ *
+ * This work is based on work done in the equipex ORTOLANG (http://www.ortolang.fr/), by several Ortolang contributors (mainly CNRTL and SLDR)
+ * ORTOLANG is funded by the French State program "Investissements d'Avenir" ANR-11-EQPX-0032
+ * %%
+ * Copyright (C) 2013 - 2015 Ortolang Team
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+
+import fr.ortolang.diffusion.event.EventServiceException;
+import fr.ortolang.diffusion.registry.KeyNotFoundException;
+import fr.ortolang.diffusion.security.authorisation.AccessDeniedException;
+import fr.ortolang.diffusion.seo.SeoService;
+import fr.ortolang.diffusion.seo.SeoServiceException;
+import fr.ortolang.diffusion.store.json.JsonStoreServiceException;
+
+import javax.ejb.EJB;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Path("/seo")
+public class SeoResource {
+
+    private static final Logger LOGGER = Logger.getLogger(SeoResource.class.getName());
+
+    @EJB
+    private SeoService seo;
+
+    @GET
+    @Produces("application/xml")
+    @Path("/sitemap")
+    public Response getEventFeed()
+            throws KeyNotFoundException, AccessDeniedException, EventServiceException, JsonStoreServiceException, TransformerException, ParserConfigurationException, SeoServiceException {
+        LOGGER.log(Level.INFO, "GET /sitemap/");
+        String siteMap = seo.generateSiteMap();
+        return Response.ok(siteMap).build();
+    }
+}
