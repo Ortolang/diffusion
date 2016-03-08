b6e217f5730ca8ab11535af076154ff2307038d1
cyrilpestel <cyril.pestel@gmail.com>
2016-03-08 11:09:04 +0100

    Checks owner and members

--- client/src/main/java/fr/ortolang/diffusion/client/cmd/CheckBagCommand.java
+++ client/src/main/java/fr/ortolang/diffusion/client/cmd/CheckBagCommand.java
@@ -1,12 +1,17 @@
 package fr.ortolang.diffusion.client.cmd;
 
+import java.io.Console;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.FileVisitResult;
 import java.nio.file.FileVisitor;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Properties;
+
+import javax.json.JsonObject;
 
 import org.apache.commons.cli.BasicParser;
 import org.apache.commons.cli.CommandLine;
@@ -15,15 +20,22 @@ import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
+import fr.ortolang.diffusion.client.OrtolangClient;
+import fr.ortolang.diffusion.client.OrtolangClientException;
+import fr.ortolang.diffusion.client.account.OrtolangClientAccountException;
+
 public class CheckBagCommand extends Command {
 
     private Options options = new Options();
     private StringBuilder errors = new StringBuilder();
     private StringBuilder fixed = new StringBuilder();
     private boolean fix = false;
+    private OrtolangClient client;
 
     public CheckBagCommand() {
         options.addOption("h", "help", false, "show help.");
+		options.addOption("U", "username", true, "username for login");
+		options.addOption("P", "password", true, "password for login");
         options.addOption("p", "path", true, "path of the bag root");
         options.addOption("f", "fix", false, "fix problems (WARNING may delete some files)");
     }
@@ -33,6 +45,8 @@ public class CheckBagCommand extends Command {
         CommandLineParser parser = new BasicParser();
         CommandLine cmd;
         String root = "";
+		String username = "";
+		String password = null;
         try {
             cmd = parser.parse(options, args);
             if (cmd.hasOption("h")) {
@@ -49,6 +63,25 @@ public class CheckBagCommand extends Command {
                 help();
             }
             
+            if (cmd.hasOption("U")) {
+				username = cmd.getOptionValue("U");
+				if (cmd.hasOption("P")) {
+					password = cmd.getOptionValue("P");
+				} else {
+					Console cons;
+					char[] passwd;
+					if ((cons = System.console()) != null && (passwd = cons.readPassword("[%s]", "Password:")) != null) {
+					    password = new String(passwd);
+					}
+				}
+			}
+			client = OrtolangClient.getInstance();
+			if ( username.length() > 0 ) {
+				client.getAccountManager().setCredentials(username, password);
+				client.login(username);
+			}
+			System.out.println("Connected as user: " + client.connectedProfile());
+			
             if ( !Files.exists(Paths.get(root)) ) {
                 errors.append("-> Le chemin de base (").append(root).append(") n'existe pas\r\n");
             } else {
@@ -57,6 +90,8 @@ public class CheckBagCommand extends Command {
                 }
                 if ( !Files.exists(Paths.get(root, "data", "workspace.properties")) ) {
                     errors.append("-> workspace.properties NOT found\r\n");
+                } else {
+                	checkWorkspaceProperties(Paths.get(root, "data", "workspace.properties"));
                 }
                 
                 if ( Files.exists(Paths.get(root, "data", "snapshots")) ) {
@@ -81,7 +116,10 @@ public class CheckBagCommand extends Command {
         } catch (ParseException | IOException e) {
             System.out.println("Failed to parse command line properties: " + e.getMessage());
             help();
-        } 
+        } catch (OrtolangClientException | OrtolangClientAccountException e) {
+			System.out.println("Unexpected error !!");
+			e.printStackTrace();
+		}
     }
     
     private void checkSnapshotMetadata(Path root) {
@@ -127,6 +165,34 @@ public class CheckBagCommand extends Command {
         }
     }
 
+    private void checkWorkspaceProperties(Path workspaceFilePath) throws IOException, OrtolangClientException, OrtolangClientAccountException {
+        Properties props = new Properties();
+        InputStream in = Files.newInputStream(workspaceFilePath);
+        props.load(in);
+        //TODO check whether workspace alias is availabled
+        String owner = props.getProperty("owner");
+        if(owner!=null) {
+        	checkObject(owner, "owner");
+        }
+        String members = props.getProperty("members");
+        if(members!=null) {
+        	for(String member : members.split(",")) {
+        		checkObject(member, "member");
+        	}
+        }
+    }
+    
+    private void checkObject(String key, String subject) throws OrtolangClientAccountException {
+		try {
+			JsonObject object = client.getObject(key);
+			if(object==null) {
+				errors.append("-> ").append(subject).append(" ").append(key).append(" doesn't exist\r\n");
+			}
+		} catch (OrtolangClientException e) {
+			System.out.println("client execption : "+e.getMessage());
+		}
+    }
+    
     private void help() {
         HelpFormatter formatter = new HelpFormatter();
         formatter.printHelp("Check Bag", options);
