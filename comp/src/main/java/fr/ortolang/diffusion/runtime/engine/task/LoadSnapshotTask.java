3e343fce57be69615753e2393e3525157334eccb
Frédéric Pierre <fpierre@atilf.fr>
2016-05-02 12:53:08 +0200

    - Compute dataSize and publicationDate and update item metadata when requesting a publication (Closes #693)

--- comp/src/main/java/fr/ortolang/diffusion/runtime/engine/task/LoadSnapshotTask.java
+++ comp/src/main/java/fr/ortolang/diffusion/runtime/engine/task/LoadSnapshotTask.java
@@ -36,39 +36,46 @@ package fr.ortolang.diffusion.runtime.engine.task;
  * #L%
  */
 
+import java.io.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 import javax.transaction.Status;
 
+import fr.ortolang.diffusion.OrtolangException;
+import fr.ortolang.diffusion.core.*;
+import fr.ortolang.diffusion.core.entity.*;
+import fr.ortolang.diffusion.store.binary.BinaryStoreServiceException;
+import fr.ortolang.diffusion.store.binary.DataCollisionException;
+import fr.ortolang.diffusion.store.binary.DataNotFoundException;
 import org.activiti.engine.delegate.DelegateExecution;
 
 import fr.ortolang.diffusion.OrtolangObjectState;
-import fr.ortolang.diffusion.core.CoreServiceException;
-import fr.ortolang.diffusion.core.WorkspaceReadOnlyException;
-import fr.ortolang.diffusion.core.entity.SnapshotElement;
-import fr.ortolang.diffusion.core.entity.Workspace;
 import fr.ortolang.diffusion.registry.KeyNotFoundException;
 import fr.ortolang.diffusion.registry.RegistryServiceException;
 import fr.ortolang.diffusion.runtime.engine.RuntimeEngineEvent;
 import fr.ortolang.diffusion.runtime.engine.RuntimeEngineTask;
 import fr.ortolang.diffusion.runtime.engine.RuntimeEngineTaskException;
-import fr.ortolang.diffusion.security.authorisation.AccessDeniedException;
+import org.activiti.engine.impl.util.json.JSONObject;
 
 public class LoadSnapshotTask extends RuntimeEngineTask {
 
-	public static final String NAME = "Snapshot Workspace";
-	private static final Logger LOGGER = Logger.getLogger(LoadSnapshotTask.class.getName());
+    public static final String NAME = "Snapshot Workspace";
+    private static final Logger LOGGER = Logger.getLogger(LoadSnapshotTask.class.getName());
 
-	public LoadSnapshotTask() {
-	}
+    public LoadSnapshotTask() {
+    }
 
-	@Override
-	public void executeTask(DelegateExecution execution) throws RuntimeEngineTaskException {
-	    checkParameters(execution);
-		String wskey = execution.getVariable(WORKSPACE_KEY_PARAM_NAME, String.class);
-		
-		try {
+    @Override
+    public void executeTask(DelegateExecution execution) throws RuntimeEngineTaskException {
+        checkParameters(execution);
+        String wskey = execution.getVariable(WORKSPACE_KEY_PARAM_NAME, String.class);
+
+        try {
             LOGGER.log(Level.FINE, "User Transaction Status: " + getUserTransaction().getStatus());
             if (getUserTransaction().getStatus() == Status.STATUS_NO_TRANSACTION) {
                 LOGGER.log(Level.FINE, "START User Transaction");
@@ -77,72 +84,64 @@ public class LoadSnapshotTask extends RuntimeEngineTask {
         } catch (Exception e) {
             LOGGER.log(Level.SEVERE, "unable to start new user transaction", e);
         }
-        
+
         try {
-			Workspace workspace = getCoreService().readWorkspace(wskey);
-			if ( workspace.getAlias() != null && workspace.getAlias().length() > 0 ) {
-			    execution.setVariable(WORKSPACE_ALIAS_PARAM_NAME, workspace.getAlias());
-			} else {
-			    execution.setVariable(WORKSPACE_ALIAS_PARAM_NAME, wskey);
-			}
-			
-			String snapshotName;
-			String rootCollection;
-			if (!execution.hasVariable(SNAPSHOT_NAME_PARAM_NAME)) {
-				if (workspace.hasChanged()) {
-				    LOGGER.log(Level.FINE, "Snapshot name NOT provided and workspace has changed since last snapshot, generating a new snapshot");
-				    snapshotName = getCoreService().snapshotWorkspace(wskey);
-					throwRuntimeEngineEvent(RuntimeEngineEvent.createProcessLogEvent(execution.getProcessBusinessKey(), "New snapshot [" + snapshotName + "] created"));
-					execution.setVariable(SNAPSHOT_NAME_PARAM_NAME, snapshotName);
-					try {
-		                LOGGER.log(Level.FINE, "User Transaction Status: " + getUserTransaction().getStatus());
-		                if (getUserTransaction().getStatus() == Status.STATUS_NO_TRANSACTION) {
-		                    LOGGER.log(Level.FINE, "START User Transaction");
-		                    getUserTransaction().begin();
-		                }
-		            } catch (Exception e) {
-		                LOGGER.log(Level.SEVERE, "unable to start new user transaction", e);
-		            }
-		        } else {
-					LOGGER.log(Level.FINE, "Snapshot name NOT provided and workspace has not changed since last snapshot, loading latest snapshot");
-					String head = workspace.getHead();
-					String root = getRegistryService().getParent(head);
-					if ( root == null ) {
-						throwRuntimeEngineEvent(RuntimeEngineEvent.createProcessLogEvent(execution.getProcessBusinessKey(), "Unable to find the latest snapshot for this workspace"));
-						throw new RuntimeEngineTaskException("unable to find an existing snapshot to publish.");
-					}
-					boolean snapshotNameFound = false;
-					for ( SnapshotElement element : workspace.getSnapshots() ) {
-						if( element.getKey().equals(root) ) {
-							execution.setVariable(SNAPSHOT_NAME_PARAM_NAME, element.getName());
-							snapshotNameFound = true;
-							break;
-						}
-					}
-					if ( ! snapshotNameFound ) {
-						throwRuntimeEngineEvent(RuntimeEngineEvent.createProcessLogEvent(execution.getProcessBusinessKey(), "Unable to find the latest snapshot for this workspace"));
-						throw new RuntimeEngineTaskException("unable to find an existing snapshot to publish.");
-					}
-				}
-			} 
-			snapshotName = execution.getVariable(SNAPSHOT_NAME_PARAM_NAME, String.class);
-			SnapshotElement snapshot = workspace.findSnapshotByName(snapshotName);
-			if (snapshot == null) {
-				throw new RuntimeEngineTaskException("unable to find a snapshot with name " + snapshotName + " in workspace " + wskey);
-			}
-			rootCollection = snapshot.getKey();
-			
-			String publicationStatus = getRegistryService().getPublicationStatus(rootCollection);
-			if (!publicationStatus.equals(OrtolangObjectState.Status.DRAFT.value())) {
-				throw new RuntimeEngineTaskException("Snapshot publication status is not " + OrtolangObjectState.Status.DRAFT
-						+ ", maybe already published or involved in another publication process");
-			}
-			throwRuntimeEngineEvent(RuntimeEngineEvent.createProcessLogEvent(execution.getProcessBusinessKey(), "Snapshot loaded and publication status is good for publication, starting publication"));
-			
-		} catch (CoreServiceException | KeyNotFoundException | AccessDeniedException | RegistryServiceException | WorkspaceReadOnlyException e) {
-			throw new RuntimeEngineTaskException("unexpected error during snapshot task execution", e);
-		}
-        
+            Workspace workspace = getCoreService().readWorkspace(wskey);
+            if ( workspace.getAlias() != null && workspace.getAlias().length() > 0 ) {
+                execution.setVariable(WORKSPACE_ALIAS_PARAM_NAME, workspace.getAlias());
+            } else {
+                execution.setVariable(WORKSPACE_ALIAS_PARAM_NAME, wskey);
+            }
+
+            String snapshotName;
+            String rootCollection;
+            if (!execution.hasVariable(SNAPSHOT_NAME_PARAM_NAME)) {
+                LOGGER.log(Level.FINE, "Updating publicationDate and datasize fields in item metadata");
+                MetadataElement ortolangItemMetadata = getCoreService().readCollection(workspace.getHead()).findMetadataByName(MetadataFormat.ITEM);
+                InputStream metadataInputStream = core.download(ortolangItemMetadata.getKey());
+                String json = new BufferedReader(new InputStreamReader(metadataInputStream)).lines().collect(Collectors.joining("\n"));
+                metadataInputStream.close();
+                JSONObject jsonObject = new JSONObject(json);
+                DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                Date date = new Date();
+                jsonObject.put("publicationDate", dateFormat.format(date));
+                jsonObject.put("datasize", Long.toString(core.getSize(workspace.getHead()).getSize()));
+                json = jsonObject.toString();
+                String hash = getBinaryStore().put(new ByteArrayInputStream(json.getBytes()));
+                core.updateMetadataObject(wskey, "/", MetadataFormat.ITEM, hash, MetadataFormat.ITEM + ".json", false);
+
+                LOGGER.log(Level.FINE, "Snapshot name NOT provided and workspace has changed since last snapshot, generating a new snapshot");
+                snapshotName = getCoreService().snapshotWorkspace(wskey);
+                throwRuntimeEngineEvent(RuntimeEngineEvent.createProcessLogEvent(execution.getProcessBusinessKey(), "New snapshot [" + snapshotName + "] created"));
+                execution.setVariable(SNAPSHOT_NAME_PARAM_NAME, snapshotName);
+                try {
+                    LOGGER.log(Level.FINE, "User Transaction Status: " + getUserTransaction().getStatus());
+                    if (getUserTransaction().getStatus() == Status.STATUS_NO_TRANSACTION) {
+                        LOGGER.log(Level.FINE, "START User Transaction");
+                        getUserTransaction().begin();
+                    }
+                } catch (Exception e) {
+                    LOGGER.log(Level.SEVERE, "unable to start new user transaction", e);
+                }
+            }
+            snapshotName = execution.getVariable(SNAPSHOT_NAME_PARAM_NAME, String.class);
+            SnapshotElement snapshot = workspace.findSnapshotByName(snapshotName);
+            if (snapshot == null) {
+                throw new RuntimeEngineTaskException("unable to find a snapshot with name " + snapshotName + " in workspace " + wskey);
+            }
+            rootCollection = snapshot.getKey();
+
+            String publicationStatus = getRegistryService().getPublicationStatus(rootCollection);
+            if (!publicationStatus.equals(OrtolangObjectState.Status.DRAFT.value())) {
+                throw new RuntimeEngineTaskException("Snapshot publication status is not " + OrtolangObjectState.Status.DRAFT
+                        + ", maybe already published or involved in another publication process");
+            }
+            throwRuntimeEngineEvent(RuntimeEngineEvent.createProcessLogEvent(execution.getProcessBusinessKey(), "Snapshot loaded and publication status is good for publication, starting publication"));
+
+        } catch (CoreServiceException | KeyNotFoundException | RegistryServiceException | WorkspaceReadOnlyException | DataNotFoundException | OrtolangException | DataCollisionException | BinaryStoreServiceException | MetadataFormatException | InvalidPathException | PathNotFoundException | IOException e) {
+            throw new RuntimeEngineTaskException("unexpected error during snapshot task execution", e);
+        }
+
         try {
             LOGGER.log(Level.FINE, "COMMIT Active User Transaction.");
             getUserTransaction().commit();
@@ -150,17 +149,17 @@ public class LoadSnapshotTask extends RuntimeEngineTask {
         } catch (Exception e) {
             LOGGER.log(Level.SEVERE, "unable to commit active user transaction", e);
         }
-	}
-	
-	private void checkParameters(DelegateExecution execution) throws RuntimeEngineTaskException {
+    }
+
+    private void checkParameters(DelegateExecution execution) throws RuntimeEngineTaskException {
         if (!execution.hasVariable(WORKSPACE_KEY_PARAM_NAME)) {
             throw new RuntimeEngineTaskException("execution variable " + WORKSPACE_KEY_PARAM_NAME + " is not set");
         }
     }
 
-	@Override
-	public String getTaskName() {
-		return NAME;
-	}
+    @Override
+    public String getTaskName() {
+        return NAME;
+    }
 
 }
