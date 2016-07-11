893991df9e9f1d6fc21872b3c8eb70135cc52064
Frédéric Pierre <fpierre@atilf.fr>
2016-07-11 19:47:19 +0200

    - Update License header

--- client/src/main/java/fr/ortolang/diffusion/client/cmd/ImportWorkspaceCommand.java
+++ client/src/main/java/fr/ortolang/diffusion/client/cmd/ImportWorkspaceCommand.java
@@ -4,7 +4,7 @@ package fr.ortolang.diffusion.client.cmd;
  * #%L
  * ORTOLANG
  * A online network structure for hosting language resources and tools.
- *
+ * *
  * Jean-Marie Pierrel / ATILF UMR 7118 - CNRS / Université de Lorraine
  * Etienne Petitjean / ATILF UMR 7118 - CNRS
  * Jérôme Blanchard / ATILF UMR 7118 - CNRS
@@ -14,7 +14,7 @@ package fr.ortolang.diffusion.client.cmd;
  * Ulrike Fleury / ATILF UMR 7118 - CNRS
  * Frédéric Pierre / ATILF UMR 7118 - CNRS
  * Céline Moro / ATILF UMR 7118 - CNRS
- *
+ * *
  * This work is based on work done in the equipex ORTOLANG (http://www.ortolang.fr/), by several Ortolang contributors (mainly CNRTL and SLDR)
  * ORTOLANG is funded by the French State program "Investissements d'Avenir" ANR-11-EQPX-0032
  * %%
@@ -24,12 +24,12 @@ package fr.ortolang.diffusion.client.cmd;
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-3.0.html>.
