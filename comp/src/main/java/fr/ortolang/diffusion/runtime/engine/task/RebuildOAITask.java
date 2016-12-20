2fe50f9e9611a4b0108e41da4c7d89e85453bb78
cyril pestel <cyril.pestel@atilf.fr>
2016-12-20 16:12:22 +0100

    Commit after each build

--- comp/src/main/java/fr/ortolang/diffusion/runtime/engine/task/RebuildOAITask.java
+++ comp/src/main/java/fr/ortolang/diffusion/runtime/engine/task/RebuildOAITask.java
@@ -1,18 +1,23 @@
 package fr.ortolang.diffusion.runtime.engine.task;
 
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.transaction.Status;
+import javax.transaction.SystemException;
 
 import org.activiti.engine.delegate.DelegateExecution;
 
-import fr.ortolang.diffusion.core.AliasNotFoundException;
 import fr.ortolang.diffusion.core.CoreServiceException;
-import fr.ortolang.diffusion.oai.exception.OaiServiceException;
 import fr.ortolang.diffusion.runtime.engine.RuntimeEngineEvent;
 import fr.ortolang.diffusion.runtime.engine.RuntimeEngineTask;
 import fr.ortolang.diffusion.runtime.engine.RuntimeEngineTaskException;
 import fr.ortolang.diffusion.security.authorisation.AccessDeniedException;
 
 public class RebuildOAITask extends RuntimeEngineTask {
+
+    private static final Logger LOGGER = Logger.getLogger(RebuildOAITask.class.getName());
 
     public static final String NAME = "Rebuild OAI";
     
@@ -28,6 +33,16 @@ public class RebuildOAITask extends RuntimeEngineTask {
 	public void executeTask(DelegateExecution execution) throws RuntimeEngineTaskException {
 		report = new StringBuilder();
 		List<String> aliases = null;
+		try {
+            if (getUserTransaction().getStatus() == Status.STATUS_NO_TRANSACTION) {
+                getUserTransaction().begin();
+                LOGGER.log(Level.FINE, "starting new user transaction.");
+                report.append("[BEGIN-TRAN]\r\n");
+            }
+        } catch (Exception e) {
+            LOGGER.log(Level.SEVERE, "unable to start new user transaction", e);
+        }
+		
         try {
             aliases = getCoreService().listAllWorkspaceAlias();
         } catch (AccessDeniedException | CoreServiceException e) {
@@ -42,7 +57,22 @@ public class RebuildOAITask extends RuntimeEngineTask {
                 String wskey = getCoreService().resolveWorkspaceAlias(alias);
                 getOaiService().buildFromWorkspace(wskey);
                 report.append("+ ").append(alias).append("\r\n");
+
+                if (getUserTransaction().getStatus() == Status.STATUS_ACTIVE) {
+                    report.append("[COMMIT-TRAN]\r\n");
+                    getUserTransaction().commit();
+                    getUserTransaction().begin();
+                    report.append("[BEGIN-TRAN]\r\n");
+                }
         	} catch (Exception e) {
+        		try {
+        			report.append("[ROLLBACK-TRAN]\r\n");
+					getUserTransaction().setRollbackOnly();
+                    getUserTransaction().begin();
+                    report.append("[BEGIN-TRAN]\r\n");
+				} catch (Exception e1) {
+					LOGGER.log(Level.SEVERE, "Unable to rollback transaction", e1);
+				}
         		report.append("- ").append(alias).append("\r\n").append(e.getMessage()).append("\r\n");
         		partial = true;
         	}
