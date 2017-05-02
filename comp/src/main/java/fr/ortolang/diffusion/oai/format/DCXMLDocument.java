e6e3f2e119d54160321dd1e95aa5d4d3ee1ca888
Cyril Pestel <cyril.pestel@atilf.fr>
2017-05-02 09:16:53 +0200

    Transform OLAC to OAI_DC

--- comp/src/main/java/fr/ortolang/diffusion/oai/format/DCXMLDocument.java
+++ comp/src/main/java/fr/ortolang/diffusion/oai/format/DCXMLDocument.java
@@ -9,37 +9,37 @@ import javax.json.JsonObject;
 public abstract class DCXMLDocument extends XMLDocument {
 
 	public static final String DC_NAMESPACE = "dc";
-	public static final List<String> DC_ELEMENTS = Arrays.asList("identifier", "title", "creator", "subject", "description", "publisher", 
-			"contributor", "date", "type", "format", "source", "language", "relation", "coverage", "rights");
-	
-    public void addDcField(String name, String value) {
-        fields.add(XMLElement.createElement(DC_NAMESPACE, name, value));
-    }
+	public static final List<String> DC_ELEMENTS = Arrays.asList("identifier", "title", "creator", "subject",
+			"description", "publisher", "contributor", "date", "type", "format", "source", "language", "relation",
+			"coverage", "rights");
 
-    public void addDcMultilingualField(String name, String lang, String value) {
-        fields.add(XMLElement.createElement(DC_NAMESPACE, name, value).withAttribute("xml:lang", lang));
-    }
+	public void addDcField(String name, String value) {
+		fields.add(XMLElement.createElement(DC_NAMESPACE, name, value));
+	}
 
-    public DCXMLDocument addDCElement(String elementName, JsonObject meta) {
-    	return addDCElement(elementName, meta, elementName);
-    }
-    
-    public DCXMLDocument addDCElement(String elementName, JsonObject meta, String tagName) {
-    	if (meta.containsKey(elementName)) {
-    		JsonArray elmArray = meta.getJsonArray(elementName);
-            for(JsonObject elm : elmArray.getValuesAs(JsonObject.class)) {
-            	if (elm.containsKey("lang") && elm.containsKey("value")) {
-            		this.addDcMultilingualField(tagName, 
-                		elm.getString("lang"), 
-                		XMLDocument.removeHTMLTag(elm.getString("value")));
-            	} else {
-            		if (elm.containsKey("value")) {
-	            		this.addDcField(tagName, XMLDocument.removeHTMLTag(elm.getString("value")));
-	            	}
-            	}
-            }
-    	}
-    	return this;
-    }
-    
+	public void addDcMultilingualField(String name, String lang, String value) {
+		fields.add(XMLElement.createElement(DC_NAMESPACE, name, value).withAttribute("xml:lang", lang));
+	}
+
+	public DCXMLDocument addDCElement(String elementName, JsonObject meta) {
+		return addDCElement(elementName, meta, elementName);
+	}
+
+	public DCXMLDocument addDCElement(String elementName, JsonObject meta, String tagName) {
+		if (meta.containsKey(elementName)) {
+			JsonArray elmArray = meta.getJsonArray(elementName);
+			for (JsonObject elm : elmArray.getValuesAs(JsonObject.class)) {
+				if (elm.containsKey("lang") && elm.containsKey("value")) {
+					this.addDcMultilingualField(tagName, elm.getString("lang"),
+							XMLDocument.removeHTMLTag(elm.getString("value")));
+				} else {
+					if (elm.containsKey("value")) {
+						this.addDcField(tagName, XMLDocument.removeHTMLTag(elm.getString("value")));
+					}
+				}
+			}
+		}
+		return this;
+	}
+
 }
--- comp/src/test/java/fr/ortolang/diffusion/oai/OLACTest.java
+++ comp/src/test/java/fr/ortolang/diffusion/oai/OLACTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.fail;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.junit.Test;
 
@@ -15,16 +17,17 @@ import fr.ortolang.diffusion.util.StreamUtils;
 
 public class OLACTest {
 
-	public OLACTest() {
-	}
+	private static final Logger LOGGER = Logger.getLogger(OLACTest.class.getName());
 
 	@Test
 	public void buildFromJsonOlac() {
 		InputStream olacInputStream = getClass().getClassLoader().getResourceAsStream("json/sample-olac.json");
 		try {
 			String olac_json = StreamUtils.getContent(olacInputStream);
+			LOGGER.log(Level.INFO, "Build from json olac");
 			OLAC olac = OLACFactory.buildFromJson(olac_json);
-			checkIfPresent(olac, new String[] {"title", "description", "abstract", "coverage", "subject", "identifier"});
+			checkIfPresent(olac,
+					new String[] { "title", "description", "abstract", "coverage", "subject", "identifier" });
 		} catch (IOException e) {
 			e.printStackTrace();
 			fail(e.getMessage());
@@ -36,16 +39,17 @@ public class OLACTest {
 		InputStream dcInputStream = getClass().getClassLoader().getResourceAsStream("json/sample-dc.json");
 		try {
 			String dc_json = StreamUtils.getContent(dcInputStream);
+			LOGGER.log(Level.INFO, "Build from json DC");
 			OLAC olac = OLACFactory.buildFromJson(dc_json);
-			checkIfPresent(olac, new String[] {"title", "description", "coverage", "subject", "identifier"});
+			checkIfPresent(olac, new String[] { "title", "description", "coverage", "subject", "identifier" });
 		} catch (IOException e) {
 			e.printStackTrace();
 			fail(e.getMessage());
 		}
 	}
-	
+
 	private void checkIfPresent(OLAC olac, String[] elementsName) {
-		for(String elm : elementsName) {
+		for (String elm : elementsName) {
 			List<XMLElement> elms = olac.listFields(elm);
 			if (elms.isEmpty()) {
 				fail("Element '" + elm + "' must be present");
