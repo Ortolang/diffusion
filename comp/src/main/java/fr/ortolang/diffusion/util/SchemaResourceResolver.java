e1f56be790b6b759fcc362a1d101528bf6679a7f
Cyril Pestel <cyril.pestel@atilf.fr>
2017-10-12 23:05:07 +0200

    Loads XSD schema from classloader (ortolang/ortolang#552)

---
+++ comp/src/main/java/fr/ortolang/diffusion/util/SchemaResourceResolver.java
@@ -0,0 +1,135 @@
+package fr.ortolang.diffusion.util;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.w3c.dom.ls.LSInput;
+import org.w3c.dom.ls.LSResourceResolver;
+
+public class SchemaResourceResolver implements LSResourceResolver {
+
+    private static final Logger LOGGER = Logger.getLogger(SchemaResourceResolver.class.getName());
+
+	public static final String schema_directory = "xsd";
+	public static final String resource_separator = "/";
+	
+	public LSInput resolveResource(String type, String namespaceURI,
+	        String publicId, String systemId, String baseURI) {
+	     // note: in this sample, the XSD's are expected to be in the root of the classpath
+		LOGGER.log(Level.INFO, "resolve resource " + type + " ; " + namespaceURI + ";" + publicId + " ; " + systemId + " ; " + baseURI);
+		String[] systemIdSplit = systemId.split("/");
+		if (systemIdSplit.length > 0) {
+			LOGGER.log(Level.INFO, "Looking for " + schema_directory + resource_separator + systemIdSplit[systemIdSplit.length - 1]);
+			InputStream resourceAsStream = this.getClass().getClassLoader()
+	            .getResourceAsStream(schema_directory + resource_separator + systemIdSplit[systemIdSplit.length - 1]);
+			LOGGER.log(Level.INFO, "resource stream  : " + resourceAsStream);
+			if (resourceAsStream == null) {
+				try {
+					resourceAsStream = new URL(systemId).openStream();
+				} catch (IOException e) {
+					LOGGER.log(Level.SEVERE, e.getMessage(), e);
+				}
+			}
+			return new Input(publicId, systemId, resourceAsStream);
+		}
+		LOGGER.log(Level.INFO, "return NULL");
+		return null;
+	}
+	
+
+	public class Input implements LSInput {
+
+		private String publicId;
+
+		private String systemId;
+
+		public String getPublicId() {
+		    return publicId;
+		}
+
+		public void setPublicId(String publicId) {
+		    this.publicId = publicId;
+		}
+
+		public String getBaseURI() {
+		    return null;
+		}
+
+		public InputStream getByteStream() {
+		    return null;
+		}
+
+		public boolean getCertifiedText() {
+		    return false;
+		}
+
+		public Reader getCharacterStream() {
+		    return null;
+		}
+
+		public String getEncoding() {
+		    return null;
+		}
+
+		public String getStringData() {
+		    synchronized (inputStream) {
+		        try {
+		            byte[] input = new byte[inputStream.available()];
+		            inputStream.read(input);
+		            String contents = new String(input);
+		            return contents;
+		        } catch (IOException e) {
+		            LOGGER.log(Level.SEVERE, "Exception " + e);
+		            return null;
+		        }
+		    }
+		}
+
+		public void setBaseURI(String baseURI) {
+		}
+
+		public void setByteStream(InputStream byteStream) {
+		}
+
+		public void setCertifiedText(boolean certifiedText) {
+		}
+
+		public void setCharacterStream(Reader characterStream) {
+		}
+
+		public void setEncoding(String encoding) {
+		}
+
+		public void setStringData(String stringData) {
+		}
+
+		public String getSystemId() {
+		    return systemId;
+		}
+
+		public void setSystemId(String systemId) {
+		    this.systemId = systemId;
+		}
+
+		public BufferedInputStream getInputStream() {
+		    return inputStream;
+		}
+
+		public void setInputStream(BufferedInputStream inputStream) {
+		    this.inputStream = inputStream;
+		}
+
+		private BufferedInputStream inputStream;
+
+		public Input(String publicId, String sysId, InputStream input) {
+		    this.publicId = publicId;
+		    this.systemId = sysId;
+		    this.inputStream = new BufferedInputStream(input);
+		}
+		}
+}
--- comp/src/main/java/fr/ortolang/diffusion/util/XmlUtils.java
+++ comp/src/main/java/fr/ortolang/diffusion/util/XmlUtils.java
@@ -1,22 +1,42 @@
 package fr.ortolang.diffusion.util;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringBufferInputStream;
 import java.io.StringReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import javax.xml.transform.Source;
+import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
 
+import org.apache.commons.io.IOUtils;
+import org.w3c.dom.Document;
+import org.w3c.dom.ls.LSInput;
+import org.w3c.dom.ls.LSResourceResolver;
+import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+
+import fr.ortolang.diffusion.oai.OaiWorkerBean;
 
 public class XmlUtils {
+
 	public static void validateXml(String xml) throws SAXException, IOException {
 		SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
         Schema schema = schemaFactory.newSchema();
-
         Validator validator = schema.newValidator();
+        validator.setResourceResolver(new SchemaResourceResolver());
         validator.validate(new StreamSource(new StringReader(xml)));
 	}
 }
